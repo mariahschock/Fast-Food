16305c77d2968c00ce0f91e6aa14ef807509b169
Mariah <mariahschock@gmail.com>
2022-06-14 17:38:54 -0700

    styling

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Pick from './Pick';
 import Name from './Name';
 import CommentsForm from './CommentsForm';
 import CommentsList from './CommentsList';
+import backgroundImage from './Background.jpeg';
 
 function App() {
   const [berries, setBerries] = useState('strawberries');
@@ -26,35 +27,37 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header>
-        <h1>
+    <main style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="App">
+        <header>
+          <h1>
         Alchemy U-Pick Farm
-        </h1>
-      </header>
-      <p>Welcome to Alchemy U-Pick! Please decide ahead of time what you would like to pick. Make sure to include your name and any additional comments!</p>
-      <section className="user-info">
-        <Name setName={ setName } name={ name }/>
-        <CommentsForm handleSubmit={handleSubmit} setCommentsForm={ setCommentsForm } commentsForm={ commentsForm } />
-      </section>
-      <section className="dropdowns">
-        <BerryDropdown setBerries={ setBerries } /> <br />
-        <FruitDropdown setFruits={ setFruits } /> <br />
-        <ProduceDropdown setProduce={ setProduce } />
-      </section>
-      <section className="pictures">
-        <Pick 
-          berries={berries} 
-          fruits={fruits}
-          produce={produce} />
-      </section>
-      <section>
-        <h2>Pickers Name: {name}</h2>
-        <h3>
-          <CommentsList comments={ comments } />
-        </h3>
-      </section>
-    </div>
+          </h1>
+        </header>
+        <p>Welcome to Alchemy U-Pick! Please decide ahead of time what you would like to pick. Make sure to include your name and any additional comments!</p>
+        <section className="user-info">
+          <Name setName={ setName } name={ name }/>
+          <CommentsForm handleSubmit={handleSubmit} setCommentsForm={ setCommentsForm } commentsForm={ commentsForm } />
+        </section>
+        <section className="dropdowns">
+          <BerryDropdown setBerries={ setBerries } /> <br />
+          <FruitDropdown setFruits={ setFruits } /> <br />
+          <ProduceDropdown setProduce={ setProduce } />
+        </section>
+        <section className="pictures">
+          <Pick 
+            berries={berries} 
+            fruits={fruits}
+            produce={produce} />
+        </section>
+        <section className="displayInfo">
+          <h2>Pickers Name: {name}</h2>
+          <h3>
+            <CommentsList comments={ comments } />
+          </h3>
+        </section>
+      </div>
+    </main>
   );
 }
 
